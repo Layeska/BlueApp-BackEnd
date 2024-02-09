59d5c7df212ad95122653ae6adfd07f701021c1d
layeska <lisa13suarez@gmail.com>
2024-02-08 21:20:52 -0600

    ✔️ Agregando conecxión a MongoDB

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-import { Socket } from "socket.io";
+import mongoose from "mongoose";
 import { server } from "./app.js";
 import { io } from "./src/utils/socketServer.js";
 
 const PORT = process.env.PORT || 3000;
 const IP_SERVER = process.env.IP_SERVER || "localhost";
+const DB_USER = process.env.DB_USER || "admin";
+const DB_PASSWORD = process.env.DB_PASSWORD || "VY5EBwZ6wHFW3o1d";
+const DB_HOST = process.env.DB_HOST || "lueapp.cpzzgv6.mongodb.net";
 
-server.listen(PORT, () => {
-    console.log("**************************************");
-    console.log("******* Welcome the Blue App *********");
-    console.log("**************************************");
-    console.log(`\nOpen in: http://${IP_SERVER}/:${PORT}\n`)
+const mongoDbUrl = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`
 
-    io.sockets.on("connection", (socket) => {
-        console.log("Nuevo usuario ha ingresado! 👋");
+mongoose.connect(mongoDbUrl)
+    .then(() => {
+        server.listen(PORT, () => {
+            console.log("**************************************");
+            console.log("******* Welcome the Blue App *********");
+            console.log("**************************************");
+            console.log(`\nLevantado en: http://${IP_SERVER}:${PORT}\n`);
 
-        socket.on("disconnect", () => {
-            console.log("USUARIO SE HA IDO ✖️");
-        })
+            io.sockets.on("connection", (socket) => {
+                console.log("Nuevo usuario ha ingresado! 👋");
 
-        Socket.on("subcribe", (room) => {
-            socket.join(room);
+                socket.on("disconnect", () => {
+                    console.log("USUARIO SE HA IDO ✖️");
+                });
+
+                socket.on("subcribe", (room) => {
+                    socket.join(room);
+                });
+
+                socket.io("unsubscribe", (room) => {
+                    socket.leave(room);
+                });
+            });
         });
-
-        socket.io("unsubscribe", (room) => {
-            socket.leave(room);
-        });
+    })
+    .catch(error => {
+        console.error("Error al momento de conectar con MongoDB:", error);
     });
-});
