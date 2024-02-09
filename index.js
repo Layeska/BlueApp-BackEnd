967e39a03f3e9beb9a5f6d639d253225c9e96fa7
layeska <lisa13suarez@gmail.com>
2024-02-08 20:17:03 -0600

    ✔️ Creando Socket

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("log")
+import { Socket } from "socket.io";
+import { server } from "./app.js";
+import { io } from "./src/utils/socketServer.js";
+
+const PORT = process.env.PORT || 3000;
+const IP_SERVER = process.env.IP_SERVER || "localhost";
+
+server.listen(PORT, () => {
+    console.log("**************************************");
+    console.log("******* Welcome the Blue App *********");
+    console.log("**************************************");
+    console.log(`\nOpen in: http://${IP_SERVER}/:${PORT}\n`)
+
+    io.sockets.on("connection", (socket) => {
+        console.log("Nuevo usuario ha ingresado! 👋");
+
+        socket.on("disconnect", () => {
+            console.log("USUARIO SE HA IDO ✖️");
+        })
+
+        Socket.on("subcribe", (room) => {
+            socket.join(room);
+        });
+
+        socket.io("unsubscribe", (room) => {
+            socket.leave(room);
+        });
+    });
+});
